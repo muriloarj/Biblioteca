1dca021a344154b1ede44561c4e249fa3098c7d6
Murilo Araújo <muriloaraujod1@gmail.com>
2026-04-10 01:51:02 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import fastapi
+from api.routes.leitores import router as leitores_router
+from api.routes.livros import router as livros_router
+
+app = FastAPI(title="Biblioteca API")
+
+@app.get("/")
+    def home():
+        return {"msg": "Biblioteca API funcionando"}
+
+app.include_router(leitores_router)
+app.include_router(livros_router)
